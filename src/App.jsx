e29e4454aa99e69041be837608809590d7fd6031
soy1040 <soy10401@gmail.com>
2026-06-16 01:33:11 +0900

    8주차과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Menu from "./pages/Menu";
-import Story from "./pages/Story";
-import NotFound from "./pages/NotFound";
+import {useState} from 'react';
+import { create } from 'zustand';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="menu" element={<Menu />} />
-        <Route path="story" element={<Story />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  );
+const useWindowStore = create((set) => ({
+    darkMode: true,
+    language: 'english',
+
+    setDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
+    setLanguage: (setLang) => set({ language: setLang }),
+}));
+
+
+export default function App() {
+    const darkMode = useWindowStore((state) => state.darkMode);
+    const language = useWindowStore((state) => state.language);
+    const setDarkMode = useWindowStore((state) => state.setDarkMode);
+    const setLanguage = useWindowStore((state) => state.setLanguage);
+
+    const windowStyle={
+        backgroundColor: darkMode ? '#222222' : '#ffffff',
+        color: darkMode ? '#ffffff' : '#000000',
+        padding: '50px',
+        height: '100vh'
+    };
+
+    return (
+        <div style={windowStyle}>
+            <h1>{language === 'english' ? 'Hello' : '안녕하세요'}</h1>
+            <div>
+                <button onClick={setDarkMode}>다크모드 변경</button>
+            </div>
+            <br />
+            <div>
+                <button onClick={() => setLanguage('korean')}>한국어</button>
+                <button onClick={() => setLanguage('english')}>English</button>
+            </div>
+        </div>
+    );
 }
-
-export default App;
