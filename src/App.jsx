69bead815f9dbb925a06fb84d650be64cc277108
온준규 <ojh6230@naver.com>
2026-06-16 14:26:06 +0900

    8th hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import TodoList from './components/TodoList';
+import { create } from "zustand";
+import "./App.css";
+
+const useSettingStore = create((set) => ({
+  language: "en",
+  darkMode: true,
+
+  changeLanguage: (newLanguage) => set({ language: newLanguage }),
+  changeDarkMode: (newDarkMode) => set({ darkMode: newDarkMode }),
+}));
 
 function App() {
+  const language = useSettingStore((state) => state.language);
+  const darkMode = useSettingStore((state) => state.darkMode);
+
+  const changeLanguage = useSettingStore((state) => state.changeLanguage);
+  const changeDarkMode = useSettingStore((state) => state.changeDarkMode);
+
   return (
-    <div className="App">
-        <TodoList/>
+    <div className={darkMode ? "app dark" : "app light"}>
+      <h1>{language === "en" ? "Hello" : "안녕하세요"}</h1>
+
+      <button onClick={() => changeDarkMode(!darkMode)}>
+        다크모드 변경
+      </button>
+
+
+      <div>
+        <button onClick={() => changeLanguage("ko")}>한국어</button>
+        <button onClick={() => changeLanguage("en")}>English</button>
+      </div>
     </div>
   );
 }
